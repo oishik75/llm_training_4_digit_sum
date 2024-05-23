Adding metrics calculation during training + Minor fixes

train.py
@@ -5,15 +5,53 @@ from transformers import TrainingArguments, Trainer
 from peft import LoraConfig, TaskType
 from peft import get_peft_model
 import torch
+import numpy as np
 
+# Tokenize dataset for training
 def tokenize(batch, tokenizer, max_length):
     training_input = [batch['prompts'][i] + str(batch['answers'][i]) for i in range(len(batch['prompts']))]
     tokenized_input = tokenizer(training_input, truncation=True, max_length=max_length, padding="max_length")
     return tokenized_input
 
+def preprocess_logits_for_metrics(logits, labels):
+    if isinstance(logits, tuple):
+        logits = logits[0]
+
+    return logits.argmax(dim=-1)
+
+def extract_answer_from_prediction(prediction):
+    # Assumes that the answer number is the first word after "Answer: "
+    # If "Answer: " is not present, or the next word is not a number assign answer to be 0
+    if "Answer: " not in prediction:
+        return 0
+    else:
+        try:
+            answer = int(prediction.split("Answer: ")[1].strip().split(" "))
+        except:
+            answer = 0
+
+    return answer
+
+# Function for computing metric for logging during evaluation
+def compute_metrics(eval_preds, metric, tokenizer):
+    pred_ids = eval_preds.predictions
+    label_ids = eval_preds.label_ids
+    # Hugginface uses -100 as the id for padding for some reason. Setting the correct padding id here that could be used to decode.
+    pred_ids[pred_ids == -100] = tokenizer.pad_token_id
+    label_ids[label_ids == -100] = tokenizer.pad_token_id
+    # Decode token_ids to text
+    pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
+    label_str = tokenizer.batch_decode(label_ids, skip_special_tokens=True)
+    pred_answers = list(map(extract_answer_from_prediction, pred_str))
+    true_answers = list(map(lambda x: int(x.split(" ")[-1]), label_str))
+    # Compute metric
+    accuracy = np.mean(np.abs(np.array(pred_answers) == np.array(true_answers)))
+    mean_absolute_error = np.mean(np.abs(np.array(pred_answers) - np.array(true_answers)))
+    return {"accuracy": accuracy, "mae": mean_absolute_error}
+
 def main():
     parser = argparse.ArgumentParser("Training Parser")
-    parser.add_argument("--model", default="google/gemma-2b", help="Mddel name or checkpoint path for base model")
+    parser.add_argument("--model", default="google/gemma-2b", help="Model name or checkpoint path for base model")
     parser.add_argument("--save_model_dir", default="models/", help="Directory for storing model checkpoints.")
     parser.add_argument("--log_dir", default="logs/", help="Directory for storing logs.")
     parser.add_argument("--data_path", default="data/", help="Directory containing train test and valid csvs.")
@@ -22,7 +60,7 @@ def main():
     # Training Arguments
     parser.add_argument("--no_cuda", action="store_true", help="Flag for not using gpu.")
     parser.add_argument("--train_batch_size", type=int, default=2, help="Training batch size.")
-    parser.add_argument("--eval_batch_size", type=int, default=2, help="Evaluation batch size.")
+    parser.add_argument("--eval_batch_size", type=int, default=8, help="Evaluation batch size.")
     parser.add_argument("--gradient_accumulation_steps", type=int, default=8, help="Number of steps for gradient accumulatation.")
     parser.add_argument("--n_epochs", type=int, default=4, help="No of training epochs.")
     parser.add_argument("--logging_steps", type=int, default=50, help="Logging steps interval")
@@ -42,6 +80,8 @@ def main():
 
     # Load Tokenizer
     tokenizer = AutoTokenizer.from_pretrained(args.model, cache_dir="cache")
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
 
     # Tokenize Data
     tokenized_dataset = dataset.map(
@@ -87,6 +127,8 @@ def main():
         tokenizer=tokenizer,
         args=training_args,
         data_collator=DataCollatorForLanguageModeling(tokenizer, mlm=False),
+        compute_metrics=lambda x: compute_metrics(x, metric="", tokenizer=tokenizer),
+        preprocess_logits_for_metrics=preprocess_logits_for_metrics,
         train_dataset=tokenized_dataset["train"],
         eval_dataset=tokenized_dataset["validation"]
     )